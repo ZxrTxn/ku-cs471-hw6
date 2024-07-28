72fdbea2a72296c026c39f362ac1622628648d99
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-07-28 23:57:30 +0700

    add DuckCall

---
+++ src/DuckCall.java
@@ -0,0 +1,11 @@
+/*
+ * Name:    Supakrit Pamakama
+ * ID:      6510450968
+ */
+
+public class DuckCall implements Quackable {
+    @Override
+    public void quack() {
+        System.out.println("Kwak");
+    }
+}

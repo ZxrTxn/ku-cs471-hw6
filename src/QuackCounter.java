dd05b428422f87aea12788f10832af5e450beea9
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-07-29 00:58:33 +0700

    add QuackCounter decorator

---
+++ src/QuackCounter.java
@@ -0,0 +1,23 @@
+/*
+ * Name:    Supakrit Pamakama
+ * ID:      6510450968
+ */
+
+public class QuackCounter implements Quackable {
+    Quackable duck;
+    static int numberOfQuacks;
+
+    public QuackCounter(Quackable duck) {
+        this.duck = duck;
+    }
+
+    @Override
+    public void quack() {
+        duck.quack();
+        numberOfQuacks++;
+    }
+
+    public static int getQuacks() {
+        return numberOfQuacks;
+    }
+}

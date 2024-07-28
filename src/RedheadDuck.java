6258c4fa504d24e875747f89096541f0e39be55f
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-07-28 23:56:52 +0700

    add RedheadDuck

---
+++ src/RedheadDuck.java
@@ -0,0 +1,11 @@
+/*
+ * Name:    Supakrit Pamakama
+ * ID:      6510450968
+ */
+
+public class RedheadDuck implements Quackable {
+    @Override
+    public void quack() {
+        System.out.println("Quack");
+    }
+}

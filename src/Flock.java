50b25661c57bb1c31581e3cfe9a2c52bc94208fb
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-07-29 13:13:04 +0700

    add duck leader

--- src/Flock.java
+++ src/Flock.java
@@ -7,14 +7,26 @@ import java.util.Iterator;
 import java.util.ArrayList;
 
 public class Flock implements Quackable {
+    Quackable leader;
     ArrayList<Quackable> quackers = new ArrayList<>();
 
     public void add(Quackable quacker) {
-        quackers.add(quacker);
+        if (leader == null) {
+            leader = quacker;
+        } else {
+            quackers.add(quacker);
+        }
     }
 
     @Override
     public void quack() {
+        if (leader != null) {
+            for (int i = 0; i < 3; i++) {
+                leader.quack();
+            }
+            System.out.println();
+        }
+
         Iterator<Quackable> iterator = quackers.iterator();
         while (iterator.hasNext()) {
             Quackable quacker = iterator.next();
